995248a5e760cd374a0e14704a69ee6cc0331d74
jungwonJung <wjdwjd1501@gmail.com>
2023-07-30 02:13:09 +0200

    add projects card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,33 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ProjectCard({ project }) {
   const { title, deployUrl, gitUrl, image } = project;
 
   return (
-    <div className="border rounded-md p-4 hover:shadow-md transition duration-300 relative">
-      <img className="h-40 w-full object-cover mb-4" src={image} alt={title} />
-      <h3 className="text-lg font-semibold mb-2">{title}</h3>
-      <div className="absolute inset-0 flex justify-center items-center opacity-0 hover:opacity-100 transition duration-300">
-        {gitUrl && (
-          <a
-            href={gitUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:text-blue-700 mr-4"
-          >
-            Git
-          </a>
-        )}
-        {deployUrl && (
-          <a
-            href={deployUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:text-blue-700"
-          >
-            Deploy
-          </a>
-        )}
+    <div className="relative rounded-md shadow-lg p-4">
+      <img
+        className="h-72 w-full object-cover mb-4 transition duration-300 hover:scale-110 hover:brightness-50"
+        src={image}
+        alt={title}
+      />
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-white opacity-0 hover:opacity-100 transition duration-300">
+        <div className="bg-black bg-opacity-50 backdrop-blur-md p-4 rounded-md w-full h-full flex flex-col justify-center items-center">
+          <h3 className="text-3xl font-semibold mb-6">{title}</h3>
+          <div className="flex flex-col items-center">
+            {gitUrl && (
+              <Link
+                to={gitUrl}
+                className="border border-brand bg-brand rounded-3xl font-medium text-xl py-2 px-4 mb-6 hover:bg-white hover:text-black hover:border-white"
+              >
+                To Git Repo
+              </Link>
+            )}
+            {deployUrl && (
+              <Link
+                to={deployUrl}
+                className="border border-brandGreen bg-brandGreen rounded-3xl font-medium text-xl py-2 px-4 mb-2 hover:bg-white hover:text-black hover:border-white"
+              >
+                To Deploy Site
+              </Link>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -5,19 +5,19 @@ import ProjectCard from "./ProjectCard";
 function ProjectList() {
   const projects = data;
   return (
-    <>
+    <section className="flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center -mt-10">
         <h1 className="text-4xl mb-10 font-semibold">My Projects</h1>
         <p className="text-xl mb-16 ">
           Here are a few past projects I've worked on.
         </p>
       </div>
-      <ul className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 ml-8">
+      <ul className="w-3/4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 ml-8 h-screen">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </ul>
-    </>
+    </section>
   );
 }
 
